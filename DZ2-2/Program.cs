57cea9ee2179ec8effa43a5fc277766de2e9a718
Vika <vigrinshpum@gmail.com>
2022-08-27 22:19:23 +0300

    DZ2-1

---
+++ DZ2-2/Program.cs
@@ -0,0 +1,13 @@
+﻿// выводит третью цифру заданного числа, или сообщает, что третьей цифры нет
+// На вход принимает трехзначное число и на выходе показывае 2ую цифру этого числа
+Console.WriteLine("Введите число: ");
+string array = Console.ReadLine();
+int a = array.Length;
+if (a>2)
+{
+    Console.WriteLine (array [2]);
+}
+else
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
