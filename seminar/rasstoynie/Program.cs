48e057d0843b48c6aad0dc88c106d84c366c4964
Vika <vigrinshpum@gmail.com>
2022-08-29 18:18:40 +0300

    rasstoynie

---
+++ seminar/rasstoynie/Program.cs
@@ -0,0 +1,15 @@
+﻿/// принимает на вход координаты2 точек т находит расстояние между ними в 2Д пространстве
+
+Console.WriteLine("Введите коордтнаты точки А");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите коордтнаты точки B");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+//int e = (x1-x2) * (x1-x2);
+//int f = (y1-y2) * (y1-y2);
+//int sum = e + f;
+//int res = Math.Sqrt (sum);
+Console.WriteLine (Math.Sqrt ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)));
+
