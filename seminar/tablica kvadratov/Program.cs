1e456cae1b22993451c8ccf974221cf17ffd912a
Vika <vigrinshpum@gmail.com>
2022-08-29 18:42:09 +0300

    таблица квадратов

---
+++ seminar/tablica kvadratov/Program.cs
@@ -0,0 +1,9 @@
+﻿//// принимает на вход число N и выводит таблица квадратов (5-> 1, 4, 9, 16, 25)
+Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= N)
+{
+    Console.Write (Math.Pow (i, 2) + "," );
+    i++;
+}
