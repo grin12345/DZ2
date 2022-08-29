7dd53cfdcb465d474a6b75c5e38c471e2943a84b
Vika <vigrinshpum@gmail.com>
2022-08-29 11:25:50 +0300

    example1

---
+++ seminar/example1/Program.cs
@@ -0,0 +1,25 @@
+﻿// gпринимает на вход координаты  (x y), причем х  и у не равны 0 и выдает № четверти
+void VyvodChetvert (int chetvert) 
+{
+Console.WriteLine ("точка принадлежит четверти №  " + chetvert);
+}
+Console.WriteLine("Введите x");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y");
+int y = int.Parse(Console.ReadLine());
+if (x>0 && y>0)
+{
+    VyvodChetvert (1);
+}
+if (x<0 && y>0)
+{
+    VyvodChetvert (2);
+}
+if (x>0 && y<0)
+{
+    VyvodChetvert (3);
+}
+if (x<0 && y<0)
+{
+    VyvodChetvert (4);
+}
