aff60a44194886f88a575022e9f5bf7c539316dc
Vika <vigrinshpum@gmail.com>
2022-08-29 11:42:47 +0300

    example2

---
+++ seminar/example2/Program.cs
@@ -0,0 +1,26 @@
+﻿// по заданному номеру четверти показывает диапозон возожных координат точек в этой четверти
+
+Console.WriteLine("Введите № четверти");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.WriteLine("x>0 y>0");
+}
+if (a ==2)
+{
+    Console.WriteLine("x<0 && y>0");
+}
+if (a ==3)
+{
+    Console.WriteLine("x>0 && y<0");
+}
+if (a ==4)
+{
+    Console.WriteLine("x<0 && y<0");
+}
+
+while (a>4)
+{Console.WriteLine("вветите число от 1 до 4");
+   a = int.Parse(Console.ReadLine());
+}
