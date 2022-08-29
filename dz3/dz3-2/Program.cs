08fb7ad7011691e1cc487e905e86a27f307b4422
Vika <vigrinshpum@gmail.com>
2022-08-29 23:15:33 +0300

    dz3-2

---
+++ dz3/dz3-2/Program.cs
@@ -0,0 +1,13 @@
+﻿/// принимает на вход координаты2 точек т находит расстояние между ними в 3Д пространстве
+
+Console.WriteLine("Введите коордтнаты точки А");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double z1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите коордтнаты точки B");
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+double z2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine (Math.Sqrt ((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2)));
