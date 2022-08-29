481f758122156138fd56cca9709e2fb05a86a1f4
Vika <vigrinshpum@gmail.com>
2022-08-29 17:38:51 +0300

    switch

---
+++ seminar/switch/Program.cs
@@ -0,0 +1,28 @@
+﻿/// по заданному номеру четверти показывает диапозон возожных координат точек в этой четверти
+
+Console.WriteLine("Введите № четверти");
+int quarter = int.Parse(Console.ReadLine());
+
+switch (quarter)
+{
+case 1:
+    {
+    Console.WriteLine ("x>0, y>0");
+    break;
+    }
+case 2:
+{
+    Console.WriteLine("x<0, y>0");
+    break;
+}
+case 3:
+{
+    Console.WriteLine("x>0, y<0");
+    break;
+}
+case 4:
+{
+    Console.WriteLine("x<0, y<0");
+    break;
+}
+}
