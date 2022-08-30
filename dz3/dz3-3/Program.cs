e6d3ece5791fa0e8c2cfaf4f72d4cf7999c4ca5f
Vika <vigrinshpum@gmail.com>
2022-08-30 11:36:45 +0300

    dz3-3

---
+++ dz3/dz3-3/Program.cs
@@ -0,0 +1,10 @@
+﻿//// принимает на вход число N и выводит таблица кубов (5-> 1, 8, 27, 64, 125)
+Console.WriteLine("Введите N");
+int n = int.Parse(Console.ReadLine());
+n = Math.Abs (n);
+int i = 1;
+while (i <= n)
+{
+    Console.Write (Math.Pow (i, 3) + "," );
+    i++;
+}
