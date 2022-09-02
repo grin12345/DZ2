bfaf1c5184b0d81f14ccc63ddbeb38f290b65ec5
Vika <vigrinshpum@gmail.com>
2022-09-02 20:56:28 +0300

    lesson3

---
+++ lesson3/array сортировка/Program.cs
@@ -0,0 +1,31 @@
+﻿// сортировка массива от мин к макс
+
+int []arr ={1,5,4,3,2,1,4,8};
+
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+    for (int i=0; i<count; i++)
+    {
+        Console.Write ($"{array[i]} ");
+    }
+    Console.WriteLine ();
+}
+
+void SelectionSort (int[] array)
+{
+       for (int i=0; i <array.Length - 1; i++)
+    {
+         int min=i;
+         for (int j = i+1; j< array.Length; j++)
+         {
+             if (array[j] < array[min]) min=j;
+         } 
+        int temp = array[i];
+        array[i] = array [min];
+        array[min] = temp;        
+    }
+}
+PrintArray (arr);
+SelectionSort (arr); 
+PrintArray (arr);
---
+++ lesson3/example2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Дан текст.В тексте нужновсе пробелы заменить черточки. 
+//Маленькие буквы к заменить большими К
+//большие С заменить маленькими с
+
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
++ "ежели бы вас послали вместо нашего милого Винценгероде,"
++ "вы бы взяли приступом согласие прусского короля."
++ "Вы так красноречивы. Вы дадите мне чаю?";
+ 
+ // string s = "qwerty"
+ //             0123
+ //s[3]  //r
+ string Replace (string text, char oldValue, char newValue)
+ {
+    string result = string.Empty;
+    int lenght = text.Length;
+    for (int i=0; i < lenght; i++)
+    {
+        if (text [i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+ }
+ string newText = Replace (text, ' ' , '|');
+ Console.WriteLine (newText);
+ Console.WriteLine ();
+ newText = Replace (text, 'к' , 'К');
+ Console.WriteLine (newText);
