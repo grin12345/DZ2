90aaed78a4799f42b49d91fb3bd53e30e3efeb30
Vika <vigrinshpum@gmail.com>
2022-09-04 19:22:46 +0300

    DZ4

---
+++ dz 4/dz4-1/Program.cs
@@ -0,0 +1,13 @@
+﻿/// Напишите цикл который принимает нв вхыд 2 число АиБ 
+// и возводит число А в нвтуральную степень Б
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int c = 1;
+for (int i=1; i<=b; i++)
+{
+    c = c*a;
+  }
+Console.WriteLine(c);
---
+++ dz 4/dz4-3/Program.cs
@@ -0,0 +1,28 @@
+﻿//задает массив из 8 элементов и выводит их на экран
+void FillArray (int [] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("[");
+    while (position < count)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+    Console.Write("]");
+ }
+
+ int []array = new int [8];
+ FillArray (array);
+ PrintArray (array);
